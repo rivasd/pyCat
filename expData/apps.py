8e5aaa3cd6d6f4e3cbdeb5a4d925cfb3e7834471
rivasd <rivasdaniel1992@gmail.com>
2016-01-28 13:40:33 -0500

    Added the manager and the data app to divide the work!

---
+++ expData/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExpdataConfig(AppConfig):
+    name = 'expData'
---
+++ expManager/__init__.py
@@ -0,0 +1,7 @@
+"""
+Django app used to take care of usual laboratory tasks and manage multiple experiments
+
+Implements subject registration/consent/additional info
+implements fetching different experiments and their configuration
+Basically the main app coordinating everything
+"""
