cb2ae35c518fab99f4e0e520180771c3bd81c8a7
rivasd <rivasdaniel1992@gmail.com>
2016-02-22 14:34:17 -0500

    tracking down source of the bug on the server...

--- pyCat/wsgi.py
+++ pyCat/wsgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 import sys
+import time
+import traceback
+import signal
 
 if sys.platform == 'linux':
     sys.path.append('/home/drivas/pyCat')
@@ -16,5 +19,12 @@ if sys.platform == 'linux':
 from django.core.wsgi import get_wsgi_application
 
 os.environ["DJANGO_SETTINGS_MODULE"]  = "pyCat.settings"
-
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+    print('WSGI without exception')
+except Exception:
+    print('handling WSGI exception')
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
